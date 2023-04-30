d866e7c82ed8fbbd31022c419717639347e53e73
Chirag Goel <engineerchirag021@gmail.com>
2023-04-30 09:13:34 +0530

    Class 7

---
+++ Class 7/index.js
@@ -0,0 +1,85 @@
+// Conditions
+
+// if
+
+// function isEven(n) {
+//     if(n % 2 === 0) {
+//         return true;
+//     }
+// }
+
+// if/else
+
+// function isEven(n) {
+//     if(n % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// if/elseif/else
+// function isEven(n) {
+//     if (n < 0) {
+//         return 'It is a negative number';
+//     } else if(n % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+
+// switch
+
+// function isEven(n) {
+//     switch(n) {
+//         case 1: 
+//         case 3:
+//         case 5:{
+//             console.log('ODD');
+//             break;
+//         }
+//         case 2: 
+//         case 4:{
+//             console.log('EVEN');
+//             break;
+//         }
+//         default: {
+//             console.log('It is a negative number');
+//         }
+//     }
+// }
+
+// isEven(-1);
+
+
+// function numberToWord(n) {
+//  // Code goes here
+// }
+
+// numberToWord(1); // ONE
+// numberToWord(2); // TWO
+
+// if/elseif/else
+// function isEven(n) {
+//     if (n < 0) {
+//         console.log('It is a negative number');
+//     } else if(n % 2 === 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+function isEven(n) {
+    if (n < 0) {
+        console.log('It is a negative number');
+    } 
+    if(n % 2 === 0) {
+        console.log(true);
+    } 
+    console.log(false);
+}
+isEven(-1);
